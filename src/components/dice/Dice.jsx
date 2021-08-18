ccfac6e212d0e86c9ae752383e66961ef7505d37
Roxana Camara <50678398+RoxanaCamara@users.noreply.github.com>
2021-08-18 14:53:02 -0300

    Error importacion

--- src/components/dice/Dice.jsx
+++ src/components/dice/Dice.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { Grid } from '@material-ui/core';
-import '../dice/Style.css'
+import '../dice/style.css'
 import Checkbox from '@material-ui/core/Checkbox';
 import { SessionContext } from '../helper/Session';
 
