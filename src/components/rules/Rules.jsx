c6f7f01617891178944b699ce3b0501fa10210fa
Roxana Camara <50678398+RoxanaCamara@users.noreply.github.com>
2022-02-11 12:51:12 -0300

    rules change text

--- src/components/rules/Rules.jsx
+++ src/components/rules/Rules.jsx
@@ -28,11 +28,9 @@ const Rules = () => {
       <CssBaseline />
       <Container component="main" className={classes.main} maxWidth="sm">
         <Typography variant="h2" component="h2" gutterBottom>
-          Como se juega ?
+          ¿Como se juega?
         </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Generala es un juego de dados que consiste en tirar 5 dados, de los dados obtenidos formar algun tipo de tirada y lograr mayor puntos que tu rival. Dentro de nuestra app, se juega en solitario por lo tanto se gana cuando no quedan mas tiradas por realizar. A continuacion detallamos las diferentes tipos de tiradas que podemos realizar. 
-          <br/>
+        <Typography variant="h5" component="h2" gutterBottom>          
           La Generala es un juego de mesa para dos o más jugadores, en el que los jugadores se turnan para lanzar 5 dados hasta 3 veces por turno, eligiendo después de cada tirada qué dados guardar y cuáles volver a tirar. El objetivo es ganar puntos haciendo ciertas combinaciones o categorías, algunas de las cuales tienen un valor fijo y otras cuya puntuación depende del valor de los dados.
         </Typography>
         {//<CollapsibleTable /> 
