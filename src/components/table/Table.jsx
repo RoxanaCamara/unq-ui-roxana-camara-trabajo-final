a2e1b372bfc2934d259e65c1c6131c6b5796e712
RoxanaCamara <roxana.camara07@gmail.com>
2022-02-12 05:28:10 -0300

    cambios tabla score

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,31 +1,39 @@
-import React, { useContext } from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box} from '@material-ui/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBan, faQuestion } from '@fortawesome/free-solid-svg-icons';
-import { SessionContext, useJugadas } from '../helper/Session';
-import TiradaCheck from './TiradaCheck';
-import '../table/style.css'
+import React, { useContext, useState } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Box,
+  Checkbox
+} from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBan, faQuestion } from "@fortawesome/free-solid-svg-icons";
+import { SessionContext, useJugadas } from "../helper/Session";
+import "../table/style.css";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
-    padding: '1rem',
-    textAlign: 'center',
-    margin: '0px'
+    padding: "1rem",
+    textAlign: "center",
+    margin: "0px",
   },
   body: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 14,
-    padding: '0.5rem',
-    margin: '0px'
+    padding: "0px",
+    margin: "0px"
   },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
   },
@@ -33,69 +41,68 @@ const StyledTableRow = withStyles((theme) => ({
 
 const useStyles = makeStyles((theme) => ({
   table: {
-    width: '18rem',  
-    paddingBottom: '3px'
-  }
-}))
-
+    width: "18rem",
+    paddingBottom: "3px",
+  },
+}));
 
 const TableScore = () => {
-
-  const { state } = useContext(SessionContext)
-  const { puntaje, eliminarJugada } = state
-  const { totalPuntaje } = useJugadas()
+  const { state } = useContext(SessionContext);
+  const { puntaje, eliminarJugada } = state;
+  const { totalPuntaje, jugadaEliminada } = useJugadas();
   const classes = useStyles();
+  const [checked, setChecked ] = useState(false);
+
+  const handleChange = (index, e) => {
+    e.preventDefault()
+    if(e.target.checked){
+      jugadaEliminada(index)
+    }
+    setChecked(!checked)
+  }
 
   return (
     <div className={classes.table}>
-    <TableContainer className='book'>
-      <Table aria-label="customized table" >
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Jugada</StyledTableCell>
-            <StyledTableCell align="center">Puntaje</StyledTableCell>
-            <StyledTableCell align="center">Anular</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {puntaje.map((row, index) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
+      <TableContainer className="book">
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Jugada</StyledTableCell>
+              <StyledTableCell align="center">Puntaje</StyledTableCell>
+              <StyledTableCell align="center">Anular</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {puntaje.map((row, index) => (
+              <StyledTableRow key={row.name}>
+                <StyledTableCell component="th" scope="row">
+                  {row.name}
+                </StyledTableCell>
+                <StyledTableCell align="center">{row.valor}</StyledTableCell>
+                <StyledTableCell align="center">
+                  {eliminarJugada ? (
+                    <Checkbox className='checkbox' checked={checked} onChange={e => handleChange(index, e)} /> 
+                  ) : row.played ? (
+                    <FontAwesomeIcon style={{margin: '0.7rem'}} icon={faBan} size="lg" />
+                  ) : (
+                    <FontAwesomeIcon style={{margin: '0.7rem'}} icon={faQuestion} size="lg" />
+                  )}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+            <StyledTableRow>
+              <StyledTableCell  component="th">
+                <Box  style={{margin: '0.7rem'}} fontWeight="fontWeightBold" fontSize={15}>
+                  Total
+                </Box>
               </StyledTableCell>
-              <StyledTableCell align="center">
-                {row.valor}
-              </StyledTableCell>
-              <StyledTableCell align="center">
-                {   
-                eliminarJugada ?
-                <TiradaCheck tirada={row} index={index} />                
-                :                  
-                    row.played ?
-                    <FontAwesomeIcon icon={faBan} size="lg" />                     
-                      :
-                      <FontAwesomeIcon icon={faQuestion} size="lg" />
-
-                }
-              </StyledTableCell>
+              <StyledTableCell align="center"  style={{margin: '0.7rem'}} >{totalPuntaje()}</StyledTableCell>
+              <StyledTableCell align="center"  style={{margin: '0.7rem'}}>-</StyledTableCell>
             </StyledTableRow>
-          ))}
-          <StyledTableRow >
-
-            <StyledTableCell component="th">
-              <Box fontWeight="fontWeightBold" fontSize={15}>
-                Total
-              </Box>
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              {totalPuntaje()}
-            </StyledTableCell>
-          </StyledTableRow>
-
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
-}
+};
 export default TableScore;
